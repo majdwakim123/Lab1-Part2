607f466edb665f08ff4910839e1a37fa7006e5a5
SamKanjow <32685926+SamKanjow@users.noreply.github.com>
2019-04-10 11:29:08 +0200

    update 11:28

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Kiss my ass");
+        System.out.println("Ready boiis");
     }
 }
